b85b3a26cc46f0b330dd69778fec577bb98fef23
Patipol Chiammunchit <Patipol Chiammunchit>
2016-01-04 11:37:28 +0700

    Add NonMajorScoreRanking table

---
+++ app/NonMajorRankingScore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NonMajorRankingScore extends Model
+{
+    //
+    public $timestamps = false;
+}
---
+++ database/migrations/2016_01_04_041816_create_non_major_ranking_scores_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNonMajorRankingScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('non_major_ranking_scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->double('me_score');
+            $table->double('ee_score');
+            $table->double('pe_score');
+            $table->double('cp_score');
+            $table->double('che_score');
+            $table->double('ce_score');
+            $table->double('ie_score');
+            $table->double('metal_score');
+            $table->double('env_score');
+            $table->double('auto_score');
+            $table->double('be_score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('non_major_ranking_scores');
+    }
+}
