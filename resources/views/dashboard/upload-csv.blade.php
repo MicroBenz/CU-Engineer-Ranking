7735fe803dce04da15dfca3b5075c6dfa8287a91
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-01-05 21:58:42 +0700

    Super mockup on Upload CSV page.

--- resources/views/dashboard/upload-csv.blade.php
+++ resources/views/dashboard/upload-csv.blade.php
@@ -9,5 +9,15 @@
 @stop
 
 @section('content')
-
+<div class="row">
+    <div class="col-lg-12">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputFile">File input</label>
+                <input type="file" id="exampleInputFile">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </div>
+</div>
 @stop
