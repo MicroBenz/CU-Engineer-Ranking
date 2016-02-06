3bd64c7e31a1597078cd4803bff6e300ffcb389f
Patipol Chiammunchit <Patipol Chiammunchit>
2016-02-06 23:02:25 +0700

    Edit Title to Subject info upload

--- resources/views/dashboard/upload-csv.blade.php
+++ resources/views/dashboard/upload-csv.blade.php
@@ -1,20 +1,21 @@
 @extends('dashboard.master-dashboard')
 
 @section('title')
-    Upload CSV
+    Upload Subject Info CSV
 @stop
 
 @section('dashboard-title')
-    Upload CSV
+    Upload Subject Info CSV
 @stop
 
 @section('content')
 <div class="row">
     <div class="col-lg-12">
-        <form>
+        <form method="post" action="{{url().'/admin/upload-csv'}}" enctype="multipart/form-data">
+            {{csrf_field()}}
             <div class="form-group">
                 <label for="exampleInputFile">File input</label>
-                <input type="file" id="exampleInputFile">
+                <input type="file" id="exampleInputFile" name="file">
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
         </form>
